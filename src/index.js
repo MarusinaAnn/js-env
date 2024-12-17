a97af2f21ea8d03cfb7984635f7eddcd598da791
Ann Marusina <an.marusina2010@yandex.ru>
2024-12-17 14:46:22 +0300

    logo

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import "./css/style.css";
 import "./js/app";
 
 // TODO: write your code in app.js
+import netologyLogo from './img/netology.svg';
+
+const img = document.createElement('img');
+img.src = netologyLogo;
+img.alt = 'Netology Logo';
+
+document.body.appendChild(img);
